b2ce750028ad778222c670e46d32beea62312106
CasedTheCoder <casedthecoder@gmail.com>
2024-06-23 18:48:38 +0200

    Añado variables básicas

---
+++ Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿int health = 100;
+float speed = 5.5f;
+char inicial = 'C';
+string nombre = "Carlos";
+bool isDead = false;
+
+Console.WriteLine("La salud de nuestro personaje es " + health);
+Console.WriteLine("La velocidad es " + speed);
+Console.WriteLine("La inicial es " + inicial);
+Console.WriteLine("El nombre es " + nombre);
+Console.WriteLine("¿Está el personaje muerto?" + isDead);
