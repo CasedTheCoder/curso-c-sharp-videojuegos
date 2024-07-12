e4447bc5972d9cebb0ca94625935f6af90103eb7
Cased The Coder <casedthecoder@gmail.com>
2024-07-12 16:51:43 +0200

    Update Program.cs

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Ejemplo de declaración de variables
 
-//int health = 100;
-//float speed = 5.5f;
-//char inicial = 'C';
-//string nombre = "Carlos";
-//bool isDead = false;
+int health = 100;
+float speed = 5.5f;
+char inicial = 'C';
+string nombre = "Carlos";
+bool isDead = false;
 
 
 // Ejemplo de imprimir por pantalla variables
 
-//Console.WriteLine("La salud de nuestro personaje es " + health);
-//Console.WriteLine("La velocidad es " + speed);
-//Console.WriteLine("La inicial es " + inicial);
-//Console.WriteLine("El nombre es " + nombre);
-//Console.WriteLine("¿Está el personaje muerto?" + isDead);
+Console.WriteLine("La salud de nuestro personaje es " + health);
+Console.WriteLine("La velocidad es " + speed);
+Console.WriteLine("La inicial es " + inicial);
+Console.WriteLine("El nombre es " + nombre);
+Console.WriteLine("¿Está el personaje muerto?" + isDead);
 
 
 /*
